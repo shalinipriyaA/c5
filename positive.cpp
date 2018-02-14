2bcdfa89a5f547411873979c837fc0ea9cf78b70
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-02-14 15:28:55 +0530

    Create positive.cpp

---
+++ positive.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int n;
+cout<<"enter a number"<<endl;
+cin>>n;
+if(n>0)
+{
+	cout<<"no is positive";
+}
+else if(n<0)
+cout<<"no is negative";
+else
+{
+	cout<<"no is zero";
+
+}
+	return 0;
+}
