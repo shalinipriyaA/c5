17cafcad95a059fe1f7281a4cf05cb00a6c2c036
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-02-15 10:51:37 +0530

    Create sumarr.cpp

---
+++ sumarr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n,k,sum;
+	int arr[n];
+cout<<"enter n,k:"<<endl;
+cin>>n>>k;
+cout<<"enter array"<<endl;
+cin>>arr[n];
+for(int i=1;i<=k;i++)
+{
+sum=sum+i;
+}
+cout<<"sum of k:"<<sum<<endl;
+	return 0;
+}
