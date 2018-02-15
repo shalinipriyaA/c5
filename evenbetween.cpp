850f9c0b16f2cb88e72f0f6f3250d11bb0f7ace9
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-02-15 13:57:28 +0530

    Create evenbetween.cpp

---
+++ evenbetween.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a,g;
+  cout<<"please enter num"<<endl;
+cin>>a>>g;
+while(a<g){
+ 
+ if(a%2==0)
+ {
+ cout<<"even no"<<a<<endl;
+  }
+ a=a+1;
+}
+}
