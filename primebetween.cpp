caf7c70d53a04d9f6164e524616880a9676c69cc
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-02-15 14:18:51 +0530

    Create primebetween.cpp

---
+++ primebetween.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b, i, flag;
+
+    cout << "Enter two numbers: "<<endl;
+    cin >>a>>b;
+       while (a<b)
+    {
+        flag = 0;
+
+        for(i = 2; i <= a/2; ++i)
+        {
+            if(a % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0)
+            cout <<a<< " ";
+        ++a;
+    }
+
+    return 0;
+}
