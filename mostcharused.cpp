fdb93a58e09ffb23be86e8360dbaae87ab935312
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-02-27 17:57:44 +0530

    Create mostcharused.cpp

---
+++ mostcharused.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#define ASCII_SIZE 256
+using namespace std;
+ 
+char getMaxOccuringChar(char* str)
+{
+       int count[ASCII_SIZE] = {0};
+ 
+        int len = strlen(str);
+    for (int i=0; i<len; i++)
+        count[str[i]]++;
+ 
+    int max = -1;  
+    char result;  
+    
+    for (int i = 0; i < len; i++) {
+        if (max < count[str[i]]) {
+            max = count[str[i]];
+            result = str[i];
+        }
+    }
+ 
+    return result;
+}
+ 
+
+int main()
+{
+    char str[] = "shalini";
+    cout << "Max occurring character is "
+         << getMaxOccuringChar(str);
+}
