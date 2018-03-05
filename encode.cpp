c9776321fd1458c31a6affa2c4c86c4f59d2b005
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-03-05 17:10:31 +0530

    Create encode.cpp

---
+++ encode.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+ 
+int main ()
+{
+	int i;
+        double original;
+	double code;
+	cout << "Enter Text: ";
+	cin >> string;
+ 
+String^ charsAndSpaces = "";
+for (int i = 0; i < chars->Length; i++)
+	charsAndSpaces += chars[i] + " ";
+ 
+cout << "Encoded Message: " << code << ".\n";
+return 0;
+}
+Edit & Run
+ 
