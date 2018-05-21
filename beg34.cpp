492662036aa2cfbce974b734795c58204548472b
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-21 13:50:28 +0530

    Create beg34.cpp

---
+++ beg34.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   char ch;
+   unsigned int long linecount, wordcount, charcount;
+   int u;
+   linecount=0;
+   wordcount=0;
+   charcount=0;
+  while((ch=getc(stdin))!=EOF){
+       if (ch !='\n') {++charcount;}
+       if (ch==' ' || ch=='\n') {++wordcount;}
+       if (ch=='\n') {++linecount;}
+}
+   if(charcount>0)
+   {
+      ++wordcount;
+      ++linecount;
+   }
+
+   printf( "%lu %lu %lu\n", charcount, wordcount, linecount );
+    return 0;
+
+}
