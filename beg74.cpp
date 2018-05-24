e46a94cd28122595c5627859cfaae9b1d18f4d1d
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-24 15:00:53 +0530

    Create beg74.cpp

---
+++ beg74.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(void)
+{
+float a,result;
+cin>>a;
+result = round(a);
+    cout << "round(" << a << ") = " << result << endl;
+return 0;
+}
