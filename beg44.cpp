851981a0cf1c82dfcd1924407a611f2a028f8081
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-24 09:31:13 +0530

    Create beg44.cpp

---
+++ beg44.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int n;
+ cout<<"enter number:"<<endl;
+ cin>>n;
+ if(n>=1)
+ {
+ if(n>=10)
+ cout<<"no"<<endl;
+ else
+ cout<<"yes"<<endl;
+ }
+	return 0;
+}
