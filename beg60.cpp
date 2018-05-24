4f0af6192c6b8ca1c079a0afda17865d3711759f
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-24 11:39:47 +0530

    Create beg60.cpp

---
+++ beg60.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  
+using namespace std;  
+int main() {  
+  int n1=0,n2=1,n3,i,a;    
+ cout<<"Enter the number of elements: ";    
+ cin>>a;    
+ cout<<n1<<" "<<n2<<" ";    
+ for(i=2;i<a;++i)   
+ {    
+  n3=n1+n2;    
+  cout<<n3<<" ";    
+  n1=n2;    
+  n2=n3;    
+ }    
+   return 0;  
+   } 
