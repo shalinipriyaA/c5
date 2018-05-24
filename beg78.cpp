0ccea98b495088e2336bd997582182cab088d451
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-24 16:20:48 +0530

    Create beg78.cpp

---
+++ beg78.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int n;
+    cout<<"Enter the number:"<<endl;
+    cin>>n;
+    if((n%13)==0)
+    {
+     cout<<"yes"<<endl;
+    }
+    else
+    {
+        cout<<" no"<<endl;
+    }
+    return 0;
+}
