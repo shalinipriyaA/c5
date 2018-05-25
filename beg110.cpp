826c89ac4d00d3265d2af490b632959160cecaab
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-25 14:11:13 +0530

    Create beg110.cpp

---
+++ beg110.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,sum=0;
+    cout<<"enter the number:"<<endl;
+    cin>>a;
+    for(int i=0;i<=a;i++)
+    {
+    sum=sum+i;
+    }
+    cout<<sum;
+    return 0;
+}
