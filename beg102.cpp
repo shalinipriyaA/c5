43204f92a111fe04c9c5faf3e8ee4b8666a64919
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-25 12:04:38 +0530

    Create beg102.cpp

---
+++ beg102.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+int a,c;
+cout<<"Enter the number..: ";
+cin>>a;
+c=a/2;
+if((a%2)==0)
+{
+cout<<c;
+}
+else
+{
+cout<<a;
+}
+	return 0;
+}
