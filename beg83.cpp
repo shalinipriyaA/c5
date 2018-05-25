80ac68ae9ec8aa585135c7f31856afc0c3d30d27
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-25 09:52:14 +0530

    Create beg83.cpp

---
+++ beg83.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+     int a,b,a1,c,d,c1;
+    cin>>a>>b;
+   
+    cin>>c>>d;
+    
+    a1=a/b;
+    c1=c%d;
+    cout<<a1<<endl;
+    cout<<c1<<endl;
+return 0;
+}
