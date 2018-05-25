4d382a6f8d66656ff277de96e620495f58cdc793
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-25 14:54:23 +0530

    Create beg.116.cpp

---
+++ beg.116.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int x,b;
+  cout<<"enter the a value:"<<endl;
+  cin>>x;
+  cout<<"enter the b value:"<<endl;
+  cin>>b;
+  cout<<x<<b;
+}
