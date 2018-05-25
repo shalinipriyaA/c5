79ff12fb41637e96419a22a07a6a0acc9ecc0b60
shalinipriyaA <36472144+shalinipriyaA@users.noreply.github.com>
2018-05-25 14:01:35 +0530

    Create beg108.cpp

---
+++ beg108.cpp
@@ -0,0 +1,12 @@
+#include<iostream>                                                                                                                                                                                   #include<iostream>
+using namespace std;
+int main()
+{
+int a,n,d;
+float sum;
+cout<<"enter the values of a,n,d";
+cin>>a>>d>>n;
+sum=a+(n-1)*d;
+cout<<"sum of AP series is:"<<sum;
+return 0;
+}
